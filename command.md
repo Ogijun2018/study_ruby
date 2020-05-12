b941d7558282b38373474987dd2baba1eb1b03af
Ogijun2018 <ogijun.mk@hotmail.co.jp>
2020-05-13 01:00:37 +0900

    first commit

---
+++ command.md
@@ -0,0 +1,6 @@
+# ruby command集
+
+## バージョンの選択方法
+確認... ruby -v, rvm list
+変更... rvm use ***(使用するrubyのバージョン)
+デフォルトにするには rvm --default use ***
---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizz_buzz(number)
+    if number % 3 == 0 && number % 5 == 0
+        "FizzBuzz"
+    elsif number % 3 == 0
+        "Fizz"
+    elsif number % 5 == 0
+        "Buzz"
+    else
+        number.to_s
+    end
+end
+
+puts fizz_buzz(20489)
